790d0017121b59b1a5ff832155e98c437e43fac1
Jean Camargo <jeancamargoreal@icloud.com>
2025-07-12 17:31:08 -0300

    Models & Enums

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Jean Camargo on 12/07/25.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
